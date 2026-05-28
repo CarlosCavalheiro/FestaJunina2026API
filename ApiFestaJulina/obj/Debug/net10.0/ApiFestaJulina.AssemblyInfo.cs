d78cb3ce39856e2607f0c98a5e78f5a57d4dee22
Carlos Alexandre Cavalheiro <58004969+CarlosCavalheiro@users.noreply.github.com>
2026-05-28 12:01:23 -0300

    Correção API

--- ApiFestaJulina/obj/Debug/net10.0/ApiFestaJulina.AssemblyInfo.cs
+++ ApiFestaJulina/obj/Debug/net10.0/ApiFestaJulina.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiFestaJulina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035679ddc74d36f4306d6f12d25ab20eebc28a05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25c6877b07ab5f2f59c02516c437e606026ade66")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiFestaJulina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiFestaJulina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
